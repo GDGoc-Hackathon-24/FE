28fe6ec9d724797156099b67468f348b6eb1946f
younghyun753 <younghyun75398@gmail.com>
2025-01-19 07:05:26 +0900

    feat: 수정

--- gdg-hackathon/next.config.ts
+++ gdg-hackathon/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://15.165.124.17:8080/api/:path*", // HTTP 백엔드 URL
+      },
+    ];
+  },
 };
 
 export default nextConfig;
